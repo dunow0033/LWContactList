392c85fa3c1306a7ec8bf1b56acc59e7f1e6b9a0
Brian Donnoe <dunow33@hotmail.com>
2023-01-19 17:42:45 -0500

    its working

---
+++ README.txt
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=aK9tOipNm0o&list=PL7ir2AVHqRHgD5hjU7y57N3120VagE2cB&index=2
+
+https://github.com/ravizworldz/RoomDbApp/tree/master/RoomDemoApp
---
+++ app/src/main/java/com/example/lwcontactlist/db/UserDao.kt
@@ -0,0 +1,19 @@
+package com.example.lwcontactlist.db
+
+import androidx.room.*
+
+@Dao
+interface UserDao {
+
+    @Query("SELECT * FROM userinfo ORDER BY id DESC")
+    fun getAllUserInfo(): List<UserEntity>?
+
+    @Insert
+    fun insertUser(user: UserEntity?)
+
+    @Delete
+    fun deleteUser(user: UserEntity?)
+
+    @Update
+    fun updateUser(userEntity: UserEntity?)
+}
